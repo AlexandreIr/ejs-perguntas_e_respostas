ba5090612727aeaf595ccc5684068c49984f1099
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-06-02 21:34:36 -0300

    feat: bootstrap and static features included

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.set('view engine','ejs');
+app.use(express.static('public'));
 
-
-app.get('/', (req, res)=>{
-    res.render();
+app.get("/", (req, res)=>{
+    res.render("index");
 });
 
 app.listen(8079, ()=>{
