dc4e31eea01fb70c05f88c4ffb40a9c2383d3932
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-06-02 22:39:05 -0300

    feat: partials and components reutilization implemented

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get("/", (req, res)=>{
     res.render("index");
 });
 
+app.get("/ask", (req, res)=>{
+    res.render("askPage");
+})
+
 app.listen(8079, ()=>{
     console.log("Aplicação iniciadda com sucesso");
 })
